6786030c1aa7f33e1bc0fcb8dad7280403d7fea2
Mariana <101410421+Placito@users.noreply.github.com>
2024-01-26 13:49:43 +0000

    trying tanslation

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
-    LANGUAGES = ['en', 'pt', 'es']
+    LANGUAGES = ['en', 'pt']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = 25
