e0e8bb47c72d33c1a9ea6de7bd358e4de18369c2
Mariana <101410421+Placito@users.noreply.github.com>
2024-01-26 12:12:32 +0000

    trying to implement translation

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
-    LANGUAGES = ['en;q=1, pt;q=0.9, es;q=0.8']
+    LANGUAGES = ['en', 'pt', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = 25
