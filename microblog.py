8eb89a4f9fb8d3fa56ac43749e40c6b5ecffd3fe
Mariana <101410421+Placito@users.noreply.github.com>
2024-01-31 19:22:15 +0000

    chapter 23

--- microblog.py
+++ microblog.py
@@ -5,7 +5,6 @@ from app.models import User, Post, Message, Notification, Task
 
 app = create_app()
 
-
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post,
