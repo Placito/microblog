6fb9635249b8d4971348dba6d197d78ab2c210e3
Mariana <101410421+Placito@users.noreply.github.com>
2024-05-08 15:39:53 +0000

    install bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from elasticsearch import Elasticsearch
@@ -24,6 +25,7 @@ login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page.')
 mail = Mail()
+bootstrap = Bootstrap()
 moment = Moment()
 babel = Babel()
 
